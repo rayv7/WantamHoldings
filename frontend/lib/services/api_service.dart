c5d21b8b56a8b0bb0b8b8a3c687d5af440319aef
Enoch Obutu <Obutuenoch@gmail.com>
2026-07-21 19:26:02 +0300

    backend integration

---
+++ frontend/lib/services/api_service.dart
@@ -0,0 +1,7 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  static const String baseUrl =
+      "https://days-nowhere-amount-empirical.trycloudflare.com/api";
+}
